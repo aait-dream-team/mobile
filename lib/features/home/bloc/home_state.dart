b0d0bc3c9b7f55c0ce7dd36971b6bfa255188ac7
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2023-05-14 14:13:49 +0300

    [feat]: implement map on homepage (#7)
    * modify version to avoid conflict with flutter_test
    
    * add search results widget
    
    * add initial map implementation.

---
+++ lib/features/home/bloc/home_state.dart
@@ -0,0 +1,35 @@
+part of 'home_bloc.dart';
+
+@immutable
+abstract class HomeState {
+  late final LatLng position;
+  late final double zoom;
+}
+
+class HomeInitialState extends HomeState {
+  @override
+  LatLng get position => LatLng(8.9806, 38.7578);
+  @override
+  double get zoom => 12.1;
+}
+
+class HomeMapState extends HomeState {
+  @override
+  final LatLng position;
+  @override
+  final double zoom;
+
+  HomeMapState(this.position, this.zoom);
+}
+
+// Map that contains a pin
+class HomePinnedState extends HomeState {
+  @override
+  final LatLng position;
+  @override
+  final double zoom;
+  final LatLng pinPosition;
+
+  HomePinnedState(
+      {required this.position, required this.zoom, required this.pinPosition});
+}
