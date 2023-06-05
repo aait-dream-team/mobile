9465449bdeaeaa4075a3c74279c864590b399a2c
Amanuel Debebe <amanueldebebe1000@gmail.com>
2023-06-05 15:47:15 +0300

    Fix merge conflict

--- lib/features/nav_detail/presentation/widgets/train_mode.dart
+++ lib/features/nav_detail/presentation/widgets/train_mode.dart
@@ -3,17 +3,44 @@ import 'package:flutter/material.dart';
 
 class WalkMode extends StatelessWidget {
   final Leg leg;
-  const WalkMode({super.key, required this.leg});
+
+  final int index;
+  final int currentIndex;
+  // final Color color;
+  const WalkMode(
+      {super.key,
+      required this.leg,
+      required this.index,
+      required this.currentIndex});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: const BoxDecoration(
+    BoxDecoration decoration;
+    if (index > currentIndex) {
+      decoration = const BoxDecoration(
+        color: Colors.white,
         border: Border(
           bottom: BorderSide(width: 1, color: Colors.grey),
         ),
-      ),
+      );
+    } else if (index < currentIndex) {
+      decoration = const BoxDecoration(
+        color: Color.fromARGB(255, 213, 210, 210),
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.grey),
+        ),
+      );
+    } else {
+      decoration = const BoxDecoration(
+        color: Color.fromARGB(255, 105, 211, 109),
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.grey),
+        ),
+      );
+    }
+    return Container(
+      height: 50,
+      decoration: decoration,
       child: const Center(
         child: Icon(Icons.directions_walk),
       ),
